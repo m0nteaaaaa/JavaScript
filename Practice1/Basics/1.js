0356ed15362c461e54e367e770698fc0aef99881
Mehul Adhikari <155364569+m0nteaaaaa@users.noreply.github.com>
2025-02-03 12:39:39 +0530

    Add files via upload

---
+++ Practice1/Basics/1.js
@@ -0,0 +1,14 @@
+//Primtive Type
+
+Number
+String
+Boolean
+null
+undefined
+
+//Number 
+ . JS has one number type
+ . Positive Numbers
+ . Negative Numbers
+ . Whole Numbers ( integers)
+ . Decimal Numbers
